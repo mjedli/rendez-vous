fb7c62a7a39358536d36543a97d7dda050ae53ce
mjedli <jedli.mejdi.recup@gmail.com>
2026-02-09 17:02:55 +0100

    login clean

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,21 +1,12 @@
 package com.example.demo;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication implements CommandLineRunner {
+public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-	@Value("${spring.data.mongodb.database}") private String dbName;
-
-
-	@Override
-	public void run(String... args) throws Exception {
-		System.out.println(">>> Base MongoDB utilisée : " + dbName);
-	}
 }
